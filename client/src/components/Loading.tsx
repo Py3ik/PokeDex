4e3a33eb1880604fd44534830affe0f647a1c55f
Py3ik <kashchko@gmail.com>
2026-05-12 14:45:55 +0200

    feature: add Loading component for displaying loading state

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-lg font-medium">Loading</h1>
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+export default Loading;
