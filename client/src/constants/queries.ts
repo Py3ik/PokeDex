80dbd18203c9f6ba178dcceca2bf803c871b7f76
Py3ik <kashchko@gmail.com>
2026-05-12 14:29:48 +0200

    feature: add collectionKeys for managing collection queries in queries.ts

--- client/src/constants/queries.ts
+++ client/src/constants/queries.ts
@@ -5,3 +5,12 @@ export const pokemonListKeys = {
       ? ([...pokemonListKeys.all, "list", params] as const)
       : ([...pokemonListKeys.all, "list"] as const),
 };
+
+export const collectionKeys = {
+  all: ["collection"] as const,
+  detail: (id: string) => [...collectionKeys.all, "detail", id] as const,
+  list: (params?: object) =>
+    params
+      ? ([...collectionKeys.all, "list", params] as const)
+      : ([...collectionKeys.all, "list"] as const),
+};
